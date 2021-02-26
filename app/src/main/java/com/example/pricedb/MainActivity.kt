1112cffd2ba299571a868b0089a29248d731e0ef
Leong Wai Yin <668915+laxa88@users.noreply.github.com>
2021-02-26 15:19:12 +0900

    add barcode scanner

--- app/src/main/java/com/example/pricedb/MainActivity.kt
+++ app/src/main/java/com/example/pricedb/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.pricedb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.activity_main.zxing_barcode_scanner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        zxing_barcode_scanner.setOnClickListener {
+            IntentIntegrator(this).initiateScan();
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
