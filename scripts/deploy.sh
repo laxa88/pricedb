466f894b8e0d13afe798d071b4063f929488850b
Leong Wai Yin <668915+laxa88@users.noreply.github.com>
2021-03-01 16:14:35 +0900

    update workflow

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
 # unused, but could be useful for the future
+# # test for required input filename
+# if [ ! -r "$1" ]; then
+#     printf "error: insufficient input or file not readable.  Usage: %s property_file\n" "$0"
+#     exit 1
+# fi
+# # `IFS` = delimiter
+# # `read` = reads line, delimited by IFS, and assigns variables in order
+# while IFS=' = ' read -r key value; do
+#     printf "$key _ $value\n"
+#     if [ "$key" == "versionCode" ]; then versionCode="$value"
+#     elif [ "$key" == "versionName" ]; then versionName="$value"
+#     fi
+# done < "$1"
+# echo $versionCode $versionName
 
-# test for required input filename
-if [ ! -r "$1" ]; then
-    printf "error: insufficient input or file not readable.  Usage: %s property_file\n" "$0"
-    exit 1
-fi
+# $foo = "s/versionCode ./versionCode 222/g"
+# bar="wow"
 
-# `IFS` = delimiter
-# `read` = reads line, delimited by IFS, and assigns variables in order
-while IFS='=' read -r key value; do
-    if [ "$key" == "version" ]; then
-        version="$value"
-    fi
-done < "$1"
-
-echo $version
+sed -i '.bak' "s/versionCode ./versionCode $2/g" $1
